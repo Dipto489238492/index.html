74fd17d6633f36879e5a37165233becc81715745
Dipto489238492 <dipunc887@gmail.com>
2026-07-20 13:18:54 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'aiims-mock-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Only cache the local files, do not cache the API requests to Google
+  if (event.request.url.includes('script.google.com')) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
